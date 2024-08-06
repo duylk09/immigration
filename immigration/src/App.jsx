840fa14cca55ab544a88358cb1caefb2b2b00c1c
Khac Duy Le <duy.lekhac12@gmail.com>
2024-08-06 21:07:45 +0100

    using Props, css, conditional rendering

--- immigration/src/App.jsx
+++ immigration/src/App.jsx
@@ -1,6 +1,10 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Food from "./Food";
+import Card from "./Card";
+import Button from "./Button";
+import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 
 function App() {
 
@@ -8,6 +12,14 @@ function App() {
     <>
       <Header/>
       <Food/>
+      <Card/>
+      <Button/>
+      {/* <UserGreeting isLoggedIn={true} username="Don" /> */}
+      <UserGreeting isLoggedIn={true}/>
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student name="Patrick" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Don" age={18} isStudent={true}/>
       <Footer/>
     </>
   );
---
+++ immigration/src/Card.jsx
@@ -0,0 +1,17 @@
+import profilePic from './assets/don-img.jpg'
+
+function Card(){
+    return(
+        <div className="card">
+            <img className='card-image' src={profilePic} alt="profile picture"></img>
+            <h2 className='card-title'>Bro code</h2>
+            <p className='card-text'>I make Youtube videos and play video games</p>
+
+        </div>
+
+
+    );
+
+}
+
+export default Card
